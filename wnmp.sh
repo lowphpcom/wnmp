aadc03e602dca8b82ac0c04bc0381c91f3606dcc
lowphpcom <lowphpcom@gmail.com>
2025-12-24 00:37:23 +0800

    v1.28
    v1.28 Added global variable wnmp; all commands can now be executed from any directory

--- wnmp.sh
+++ wnmp.sh
@@ -2792,7 +2792,7 @@ EOF
 
 cat <<'EOF' >  /usr/local/nginx/html/403.html
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
@@ -2859,7 +2859,7 @@ EOF
 
 cat <<'EOF' >  /usr/local/nginx/html/404.html
 <!DOCTYPE html>
-<html lang="zh">
+<html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
